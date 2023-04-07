6630c04a7ffa6f5328732af3c8f99d68039f06a8
soohyun <bmy1320@gmail.com>
2023-04-07 16:22:50 +0900

    :sparkles: feat: 로그인 기능 구현

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,15 @@
 import { collection, getDocs } from "firebase/firestore";
 import { Link } from "react-router-dom";
-import { fireStore } from "@/firebase";
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { firebaseAuth, fireStore } from "@/firebase";
 import { Layout } from "@/styles/style";
 
 export default function Login() {
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [user, setUser] = useState({});
+
   const useMember = async () => {
     const memberInfo = collection(fireStore, `user/`);
     const memberData = await getDocs(memberInfo);
@@ -15,12 +21,47 @@ export default function Login() {
     console.log(members);
   };
 
+  const login = async () => {
+    try {
+      const curUserInfo = await signInWithEmailAndPassword(
+        firebaseAuth,
+        userEmail,
+        userPassword
+      );
+      console.log(curUserInfo);
+      setUser(curUserInfo.user);
+      console.log(curUserInfo.user);
+    } catch (err) {
+      //   setIsAppropriate(false);
+      // console.log(err.code);
+      /*
+      입력한 아이디가 없을 경우 : auth/user-not-found.
+      비밀번호가 잘못된 경우 : auth/wrong-password.
+      */
+    }
+  };
+
   useMember();
 
   return (
     <Layout>
       <div>
         <h1>로그인 페이지입니다.</h1>
+        <div>
+          <input
+            onChange={(e) => setUserEmail(e.target.value)}
+            placeholder="이메일"
+          />
+        </div>
+        <div>
+          <input
+            onChange={(e) => setUserPassword(e.target.value)}
+            placeholder="비밀번호"
+          />
+        </div>
+        <div>
+          <button onClick={() => login()}>로그인</button>
+        </div>
         <Link to="/SignUp">
           <button>회원가입</button>
         </Link>
