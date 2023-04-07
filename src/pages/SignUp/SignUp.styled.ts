18621d59abbf02e22e62e86a518c5f14be1eb2b5
soohyun <bmy1320@gmail.com>
2023-04-07 15:58:30 +0900

    :sparkles: feat: 회원가입 페이지 세팅

---
+++ src/pages/SignUp/SignUp.styled.ts
@@ -0,0 +1,3 @@
+import styled from "styled-components";
+
+export const SignUpSection = styled.section``;
---
+++ src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,9 @@
+import { Layout } from "../Home/Home.styled";
+
+export default function SignUp() {
+  return (
+    <Layout>
+      <div>회원가입페이지입니다.</div>
+    </Layout>
+  );
+}
