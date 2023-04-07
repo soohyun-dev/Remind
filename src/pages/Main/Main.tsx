9f05713b5fb65273d2ba75e207dab692d390eac3
soohyun <bmy1320@gmail.com>
2023-04-07 16:39:51 +0900

    :sparkles: feat: 메인페이지 세팅

---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,12 @@
+import { Layout } from "../Home/Home.styled";
+import { MainSection } from "./Main.styled";
+
+export default function Main() {
+  return (
+    <Layout>
+      <MainSection>
+        <div> 메인페이지입니다!!!!</div>
+      </MainSection>
+    </Layout>
+  );
+}
