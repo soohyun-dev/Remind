7510e63e5ada2a047797b76d4dcdea33d1d00b31
soohyun <bmy1320@gmail.com>
2023-04-07 15:31:13 +0900

    :sparkles: feat: store 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import persistStore from "redux-persist/es/persistStore";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
+import store from "./store";
 
+export const persistor = persistStore(store);
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
---
+++ src/store/store.tsx
@@ -0,0 +1,21 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import persistReducer from "redux-persist/es/persistReducer";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const reducer = combineReducers({});
+
+const rootReducer = persistReducer(persistConfig, reducer);
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+export default store;
