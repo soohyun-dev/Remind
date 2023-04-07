c82bd46322f1588c723a5014053e1290caf726d0
soohyun <bmy1320@gmail.com>
2023-04-07 15:04:48 +0900

    :sparkles: feat: redux-persister 설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </QueryClientProvider>
 );
