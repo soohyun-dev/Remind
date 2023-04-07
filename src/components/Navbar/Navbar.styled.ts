ae7b7e5ad322cbe001d2ba0082a681d910eae395
soohyun <bmy1320@gmail.com>
2023-04-07 16:55:45 +0900

    :sparkles: feat: Navbar 폴더 세팅

---
+++ src/components/Navbar/Navbar.styled.ts
@@ -0,0 +1,3 @@
+import styled from "styled-components";
+
+export const NavbarSection = styled.section``;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import { NavbarSection } from "./Navbar.styled";
+
+export default function Navbar() {
+  return (
+    <NavbarSection>
+      <div>네이브바</div>
+    </NavbarSection>
+  );
+}
