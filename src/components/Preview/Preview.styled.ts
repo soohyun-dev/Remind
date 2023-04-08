d884e123c8bc2f431c6d93ba9b50df70316ad407
soohyun <bmy1320@gmail.com>
2023-04-08 16:00:20 +0900

    :sparkles: feat: Preview 컴포넌트 파일 세팅

---
+++ src/components/Preview/Preview.styled.ts
@@ -0,0 +1,3 @@
+import styled from "styled-components";
+
+export const PreviewBox = styled.div``;
---
+++ src/components/Preview/Preview.tsx
@@ -0,0 +1,11 @@
+import { PreviewBox } from "./Preview.styled";
+
+export default function Preview() {
+  return (
+    <PreviewBox>
+      <div>
+        <p>포스팅</p>
+      </div>
+    </PreviewBox>
+  );
+}
