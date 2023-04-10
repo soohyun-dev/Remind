d84445ced79f242c7cd9dffbd49757c0cfe53034
soohyun <bmy1320@gmail.com>
2023-04-10 11:02:03 +0900

    :sparkles: feat: Main 스타일링

--- src/pages/Main/Main.styled.ts
+++ src/pages/Main/Main.styled.ts
@@ -1,3 +1,39 @@
 import styled from "styled-components";
 
 export const MainSection = styled.section``;
+
+export const MainIntroduceBox = styled.div`
+  padding: 1em;
+  text-align: center;
+  min-height: 4vh;
+`;
+
+export const MainUserBox = styled.div`
+  font-size: 16px;
+  font-weight: 500;
+`;
+
+export const MainGuideBox = styled.div`
+  margin-top: 1em;
+  font-size: 20px;
+`;
+
+export const MainCatagoryBox = styled.div`
+  min-height: 1.5vh;
+  display: flex;
+  justify-content: flex-end;
+  padding: 1em;
+  border-bottom: 1px solid #d2e3fc;
+`;
+
+export const MainCatagoryButton = styled.button`
+  margin: 0 0.3em;
+  padding: 0.2em 0.8em;
+  border: 1px solid #d2e3fc;
+  border-radius: 10px;
+  background: #fff;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.9;
+  }
+`;
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -3,7 +3,14 @@ import { Suspense } from "react";
 import { Link } from "react-router-dom";
 import { selectDocId, selectNickname } from "@/feature/userSlice";
 
-import { MainSection } from "./Main.styled";
+import {
+  MainCatagoryBox,
+  MainCatagoryButton,
+  MainGuideBox,
+  MainIntroduceBox,
+  MainSection,
+  MainUserBox,
+} from "./Main.styled";
 import Navbar from "@/components/Navbar";
 import useOngoingPosting from "@/hooks/useOngoingPosting";
 import Preview from "@/components/Preview";
@@ -18,10 +25,19 @@ export default function Main() {
     <Layout>
       <Navbar />
       <MainSection>
-        <div>{nickName === null ? "" : <p>{nickName} 님</p>}</div>
-        <div>
-          <p>현재 진행중인 체험단 목록이에요!</p>
-        </div>
+        <MainIntroduceBox>
+          <MainUserBox>
+            {nickName === null ? "" : <p> 안녕하세요! {nickName} 님</p>}
+          </MainUserBox>
+          <MainGuideBox>
+            <p>현재 진행중인 체험단 목록이에요!</p>
+          </MainGuideBox>
+        </MainIntroduceBox>
+        <MainCatagoryBox>
+          <MainCatagoryButton>진행중</MainCatagoryButton>
+          <MainCatagoryButton>완료</MainCatagoryButton>
+          <MainCatagoryButton>전체</MainCatagoryButton>
+        </MainCatagoryBox>
         <Suspense fallback={<div>Loading...</div>}>
           {Object.keys(ongoingPosting).map((key: string) => (
             <Preview key={ongoingPosting[+key].id} {...ongoingPosting[+key]} />
